6d186b78103989f108966faa46640e514ae27641
kostiantyn-chechel <kostiantyn.chechel@clockwise.software>
2020-11-23 17:22:09 +0200

    refactoring authReducer JS->>TS (not completely)

--- client/src/store/reducers/authReducer.ts
+++ client/src/store/reducers/authReducer.ts
@@ -1,12 +1,18 @@
-import {AUTH_USER_MESSAGE, SET_CURRENT_USER, SET_IS_TOKEN} from '../actions/actionTypes';
+import { AUTH_USER_MESSAGE, SET_CURRENT_USER, SET_IS_TOKEN } from '../actions/actionTypes';
 
-const initialState = {
+export type AuthInitialStateType = {
+    currentUser: {},
+    message: string | null,
+    isToken: boolean,
+};
+
+const initialState: AuthInitialStateType = {
     currentUser: {},
     message: null,
     isToken: false,
 };
 
-export default function authReducer(state = initialState, action) {
+export default function authReducer(state = initialState, action: any): AuthInitialStateType { // --++--
     switch (action.type) {
         case SET_CURRENT_USER:
             return {
