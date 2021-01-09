b3ba48e905b81987918686b2b19614db3132a4fb
kostiantyn-chechel <kostiantyn.chechel@clockwise.software>
2021-01-09 15:52:03 +0200

    heroku 04-06

--- client/src/component/DataPanel/CityDataPanel.tsx
+++ client/src/component/DataPanel/CityDataPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Grid from '@material-ui/core/Grid';
+import {Grid} from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import makeStyles from '@material-ui/core/styles/makeStyles';
