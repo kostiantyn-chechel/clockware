35a953fa6d9c786e2c5053682a26bc738d1fe879
kostiantyn-chechel <kostiantyn.chechel@clockwise.software>
2021-01-13 16:05:13 +0200

    save auth User to LocalStorage

--- client/src/store/actions/authAction.ts
+++ client/src/store/actions/authAction.ts
@@ -11,61 +11,34 @@ import { postServerRequest, postAuthServerRequest } from "../../helpers/axios/ax
 
 export const userLoginFetch = (userInfo: IAuthUser) => {
     return async (dispatch: any) => postServerRequest('/auth', userInfo)
-        .then(response => {
-            if (response.token) {
-                dispatch({ type: SET_USER, payload: response });
-                saveToken(response.token, response.status);
-                // saveUserToLocalStorage({response.name, response.status}); // TODO согласовать возвращаемые типы
-            } else {
-                if(response.message) {
-                    dispatch(authUserMessage(response.message));
-                }
-            }
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
+        .then(response => setUser(response, dispatch))
+        .catch(err => console.log(err.message))
 };
 
 export const userRegistrationFetch = (userRegInfo: IRegUser) => {
     return async (dispatch: any) => postServerRequest('/auth/reg', userRegInfo)
-        .then(response => {
-            if (response.token) {
-                dispatch({ type: SET_USER, payload: response });
-                saveToken(response.token, response.status);
-                // saveUserToLocalStorage(response); // TODO согласовать возвращаемые типы
-            } else {
-                if(response.message) {
-                    console.log('registration: ',response.message);
-                    dispatch(authUserMessage(response.message));
-                }
-            }
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
+        .then(response => setUser(response, dispatch))
+        .catch(err => console.log(err.message))
 };
 
 export const userRegistrationChange = (userChangeRegInfo: IChangeRegUser) => {
     return async (dispatch: any) => postAuthServerRequest('/auth/change', userChangeRegInfo)
-        .then(response => {
-            if (response.token) {
-                dispatch({ type: SET_USER, payload: response });
-                saveToken(response.token, response.status);
-                saveUserToLocalStorage(response); // TODO согласовать возвращаемые типы
-            } else {
-                if(response.message) {
-                    console.log('registration: ',response.message);
-                    // dispatch(authUserMessage(response.message));
-                }
-            }
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
+        .then(response => setUser(response, dispatch))
+        .catch(err => console.log(err.message))
 };
 
-
+export const setUser = (response: any, dispatch: any) => {
+    if (response.token) {
+        dispatch(setAuthUser(response));
+        saveToken(response.token, response.status);
+        saveUserToLocalStorage(response);
+    } else {
+        if(response.message) {
+            dispatch(authUserMessage(response.message));
+        }
+    }
+};
+export const setAuthUser = (user) => ({ type: SET_USER, payload: user });
 export const authUserMessage = (message: string): AuthActionTypes => ({ type: AUTH_USER_MESSAGE, payload: message });
 export const resetUser = () => ({ type: RESET_USER });
 export const setUserStatus = (status: TUserStatus) => ({ type: SET_USER_STATUS, payload: status });
