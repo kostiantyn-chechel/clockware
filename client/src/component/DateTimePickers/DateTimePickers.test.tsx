d24669cd257f0bf3fa39123b18067030311bde72
Kostiantyn Chechel <kostiantyn.chechel@clockwise.software>
2021-03-09 18:51:18 +0200

    test DateTimePikers to Date earlier than current

--- client/src/component/DateTimePickers/DateTimePickers.test.tsx
+++ client/src/component/DateTimePickers/DateTimePickers.test.tsx
@@ -6,9 +6,16 @@ configure({ adapter: new Adapter() });
 
 import DateTimePickers from './DateTimePickers';
 
+// jest.mock('../../helpers/dateTime', () => ({
+//     nowTimePlus: () => '13:00',
+//     today: () => '2021-02-02',
+// }));
+
 describe('Test <DateTimePickers>', () => {
     const handleSelectDateMock = jest.fn();
     const handleSelectTimeMock = jest.fn();
+    const selectDay = jest.fn(() => '2021-02-03');
+    const selectTime = jest.fn(() => '15:00');
 
     const Component = <DateTimePickers
         handleSelectDate={handleSelectDateMock}
@@ -27,8 +34,6 @@ describe('Test <DateTimePickers>', () => {
 
     test('Changes value after change Data', () => {
         const MountComponent = mount(Component);
-        // const instance = MountComponent.instance();
-        // instance.setChoiceDataTime('2021-02-23');
 
         MountComponent.find('#date-label').first().simulate('change', {
             target: { value: '2021-02-02' },
@@ -38,5 +43,24 @@ describe('Test <DateTimePickers>', () => {
 
         // expect(handleSelectDateMock).toHaveBeenCalledWith('2021-02-02');
     });
+
+    describe('Date and time earlier than current', function () {
+
+        test('Error message expected', () => {
+            const MountComponent = mount(Component);
+
+            MountComponent.find('#date-label').first().simulate('change', {
+                target: { value: '2021-01-02' },
+            });
+
+            const node = MountComponent.find('.makeStyles-warningBlock-43');
+
+            expect(node).toBeDefined();
+
+        });
+    });
+
+
+
 
 });
